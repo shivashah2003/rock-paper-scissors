447fe192bd36fd25a40ff9a8c31d1f7fbd203436
Shiva Kumar <shivajiraw1122003@gmail.com>
2024-08-17 12:27:56 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,88 @@
+let score = JSON.parse(localStorage.getItem('score')) || { 
+  wins: 0, 
+  loses: 0, 
+  ties: 0
+};
+
+updateScoreElement();
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+
+  let result = '';
+
+  if(playerMove === 'rock'){
+    if(computerMove === 'rock'){
+      result = 'Tie.';
+    } else if(computerMove === 'paper'){
+      result = 'You lose.';
+    } else if(computerMove === 'scissors'){
+      result = 'You win.';
+    }
+
+  } else if(playerMove === "paper"){
+    if(computerMove === 'rock'){
+      result = 'You win.';
+    } else if(computerMove === 'paper'){
+      result = 'Tie.';
+    } else if(computerMove === 'scissors'){
+      result = 'You lose.';
+    }
+
+  } else if(playerMove === 'scissors'){
+    if(computerMove === 'rock'){
+      result = 'You lose.';
+    } else if(computerMove === 'paper'){
+      result = 'You win.';
+    } else if(computerMove === 'scissors'){
+      result = 'Tie.';
+    }
+  }
+
+  if(result === 'You win.') {
+    score.wins += 1;
+
+  } else if(result === 'You lose.') {
+    score.loses += 1;
+
+  } else if(result === 'Tie.') {
+    score.ties += 1;
+  }
+
+  localStorage.setItem('score',JSON.stringify(score));
+  updateScoreElement();
+
+  document.querySelector('.js-result').innerHTML = result;
+
+  document.querySelector('.js-moves').innerHTML = `You <img class="click-icon" src="images/${playerMove}-emoji.png"> - <img class="click-icon" src="images/${computerMove}-emoji.png"> Computer`;
+
+}
+
+function pickComputerMove(){
+  
+  let computerMove = '';
+  const randomNumber = Math.random();
+
+  if (randomNumber >= 0 && randomNumber < 1/3){
+    computerMove = 'rock';
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3){
+    computerMove = 'paper';
+  } else if (randomNumber >= 2/3 && randomNumber < 1){
+    computerMove = 'scissors';
+  }
+
+  return computerMove;
+}
+
+function updateScoreElement() {
+  document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Loses: ${score.loses}, Ties: ${score.ties}`;
+
+}
+
+function updateResult() {
+  document.querySelector('.js-result').innerHTML = result;
+}
+
+function updateMove() {
+  document.querySelector('.js-moves').innerHTML = `You <img class="click-icon" src="images/${playerMove}-emoji.png"> - <img class="click-icon" src="images/${computerMove}-emoji.png Computer ">`;
+}
